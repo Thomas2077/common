ac5b40d7bdb29f805b08ba98b3360fca033fb189
JackZouShao <jackzouyan@gmail.com>
2025-04-15 00:50:22 +0900

    error handle

--- src/main/kotlin/com/thomas/common/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/thomas/common/exception/GlobalExceptionHandler.kt
@@ -1,10 +1,9 @@
 package com.thomas.common.exception
 
 import com.thomas.common.log.logger
+import com.thomas.common.response.ErrorResponse
 import jakarta.servlet.http.HttpServletRequest
-import org.springframework.http.HttpRequest
 import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -17,22 +16,36 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
  **/
 @RestControllerAdvice
 class GlobalExceptionHandler {
-    @ExceptionHandler(RuntimeException::class)
-    fun handleRuntimeException(ex: RuntimeException, request: HttpServletRequest): ResponseEntity<String> {
-        val url = request.requestURL.toString()
-        logger().error(url + " : " + ex.message)
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("")
-    }
+
 
     @ExceptionHandler
-    fun handleDataNotFoundException(ex: DataNotFoundException): ResponseEntity<String> {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("")
+    fun handleDataNotFoundException(
+        ex: DataNotFoundException,
+        request: HttpServletRequest
+    ): ResponseEntity<ErrorResponse> {
+        val url = request.requestURL.toString()
+        logger().error(url + " : " + ex.message)
+        return ResponseEntity
+            .status(HttpStatus.NOT_FOUND)
+            .body(ErrorResponse(400004, ex.message, null, url, System.currentTimeMillis()))
+    }
+
+    @ExceptionHandler(RuntimeException::class)
+    fun handleRuntimeException(ex: RuntimeException, request: HttpServletRequest): ResponseEntity<ErrorResponse> {
+        val url = request.requestURL.toString()
+        logger().error(url + " : " + ex.message)
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(ErrorResponse(500001, ex.message, null, url, System.currentTimeMillis()))
     }
 
     @ExceptionHandler(Exception::class)
-    fun handleException(ex: Exception, request: HttpServletRequest): ResponseEntity<String> {
+    fun handleException(ex: Exception, request: HttpServletRequest): ResponseEntity<ErrorResponse> {
+
         val url = request.requestURL.toString()
         logger().error(url + " : " + ex.message)
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("")
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(ErrorResponse(500000, ex.message, null, url, System.currentTimeMillis()))
     }
 }
--- src/main/kotlin/com/thomas/common/response/ErrorResponse.kt
+++ src/main/kotlin/com/thomas/common/response/ErrorResponse.kt
@@ -1,17 +1,15 @@
 package com.thomas.common.response
 
-import java.sql.Timestamp
-
 /**
  * @author yanzou
  * @date   2025/4/13
  * @version V1.0.0
  * @description
  **/
-data class ApiResponse<T>(
+data class ErrorResponse(
     var code: Int = 200,
-    var message: String = "success",
-    var data: T? = null,
+    var message: String? = "success",
+    var details: MutableList<String>? = null,
     var url: String? = null,
     val timestamp: Long = System.currentTimeMillis()
 )
